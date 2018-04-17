c835827059b4fd6442ab56a1ba44118fa7d9136e
BigBen03 <38051365+BigBen03@users.noreply.github.com>
2018-04-17 17:39:31 -0400

    With exception handling and changed lines

--- json_cmd_table.cpp
+++ json_cmd_table.cpp
@@ -1,7 +1,7 @@
 /*
  * json_table.cpp
  *
- * Benjamin Lanthier and Kevin De-Oliveira
+ * Benjamin Lanthier and Kevin De Oliveira
  */
 
 #include <iostream>
@@ -9,9 +9,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <nlohmann/json.hpp>
+#include "json.hpp"
+#include "json_exception.h"
+
 
 using namespace std;
+using namespace JException;	//end of file json_exception.h
 
 using json = nlohmann::json;
 
@@ -23,42 +26,51 @@ json getjFile(string);
 vector<vector<string>> getVector(json);
 vector<vector<string>> sortByInput(vector<vector<string>>, string);
 vector<vector<string>> selectionSortVector(vector<vector<string>>);
-
-
 //==================================================================================
 //Main function
 
 int main(int argc, char *argv[]) {
 
-	cout<< argc << endl;
-	cout << argv[0] << endl;
-	cout << argv[1] << endl;
-	cout << argv[2] << endl;
-	cout << argv[3] << endl;
-	cout << argv[4] << endl;
+	try{
+		//exits program if wrong number of input arguments
+		if (argc != 5)
+			throw InvalidArgumentsException();
 
-	//exits program if wrong number of input arguments
-	if (argc != 5) {
-		ofstream err("Error.txt");
-		err << "Wrong number of input arguments in the command line";
-		err.close();
+
+		cout<< "Number of Arguments: " << argc << endl
+			<< "List of Arguments:\n"
+			<< argv[0] << endl
+			<< argv[1] << endl
+			<< argv[2] << endl
+			<< argv[3] << endl
+			<< argv[4] << endl;
+
+		json jfile;
+
+		//Transforms char* to string for ease of use
+		string input = static_cast<string> (argv[3]);
+		string output = static_cast<string> (argv[4]);
+
+		//Looks for an input {Sample_name}.json
+		if (input.find(".json") != std::string::npos)
+		jfile = getjFile(input.substr(input.find_last_of("\\") + 1));
+
+		else
+			throw FilePathException();
+
+		//Looks for an output sortby{input_name}.table
+		if (output.find(".table") != std::string::npos)
+		buildTable(jfile, output.substr(output.find_last_of("\\") + 1), argv[2]);
+
+		else
+			throw FilePathException();
+
+	}catch(exception &e){
+		cout<<"Exception has been caught: "<<e.what()<<endl;
 		return 1;
 	}
-
-	//Transforms char* to string for ease of use
-	string input = static_cast<string> (argv[3]);
-	string output = static_cast<string> (argv[4]);
-
-	//Looks for an input Sample.json
-	json jfile = getjFile(input.substr(input.find("Sample.json"), 11));
-
-	//Looks for an output sortbyid.table
-	buildTable(jfile, output.substr(output.find("sortbyid.table"), 14), argv[2]);
-
 	return 0;
 }
-
-
 
 //==================================================================================
 //Function definitions
@@ -83,80 +95,77 @@ void buildTable(json jfile, string outfile, string outSort) {
 
 	//Each sort type has an output loop such that they are well formatted
 		if (outSort == "id") {
-			out << setw(72) << "|  id  |  type  |  name  |     batter     |           topping          |"<< endl;
-			out << setw(72) << "------------------------------------------------------------------------"<< endl;
+			out << setw(80) << "|  id  |    type    |    name    |     batter     |           topping          |"<< endl;
+			out << setw(80) << "--------------------------------------------------------------------------------"<< endl;
 
 			for (unsigned int it = 0; it < jsonVector.size(); it++) {
 				out << "| " << setw(4) << left << jsonVector[it][0] << " |"
-					<< setw(8) << right << jsonVector[it][1] << "|"
-					<< setw(8) << right << jsonVector[it][2] << "|"
+					<< setw(12) << right << jsonVector[it][1] << "|"
+					<< setw(12) << right << jsonVector[it][2] << "|"
 					<< setw(16) << right << jsonVector[it][3] << "|"
 					<< setw(28) << right << jsonVector[it][4] << "|" << endl
-					<< setw(72) << "------------------------------------------------------------------------" << endl;
+					<< setw(80) << "--------------------------------------------------------------------------------" << endl;
 			}
 		}
 
 		else if (outSort == "type") {
-			out << setw(72) << "|  type  |  id  |  name  |     batter     |           topping          |"<< endl;
-			out << setw(72) << "------------------------------------------------------------------------"<< endl;
+			out << setw(80) << "|    type    |  id  |    name    |     batter     |           topping          |"<< endl;
+			out << setw(80) << "--------------------------------------------------------------------------------"<< endl;
 
 			for (unsigned int it = 0; it < jsonVector.size(); it++) {
-				out << "|" << setw(8) << right << jsonVector[it][0]
+				out << "|" << setw(12) << right << jsonVector[it][0]
 					<< "| " << setw(4) << left << jsonVector[it][1] << " |"
-					<< setw(8) << right << jsonVector[it][2] << "|"
+					<< setw(12) << right << jsonVector[it][2] << "|"
 					<< setw(16) << right << jsonVector[it][3] << "|"
 					<< setw(28) << right << jsonVector[it][4] << "|" << endl
-					<< setw(72) << "------------------------------------------------------------------------" << endl;
+					<< setw(80) << "--------------------------------------------------------------------------------" << endl;
 			}
 		}
 
 		else if (outSort == "name") {
-			out << setw(72) << "|  name  |  type  |  id  |     batter     |           topping          |"<< endl;
-			out << setw(72) << "------------------------------------------------------------------------"<< endl;
+			out << setw(80) << "|    name    |    type    |  id  |     batter     |           topping          |"<< endl;
+			out << setw(80) << "--------------------------------------------------------------------------------"<< endl;
 
 			for (unsigned int it = 0; it < jsonVector.size(); it++) {
-				out << "|" << setw(8) << right << jsonVector[it][0] << "|"
-					<< setw(8) << right << jsonVector[it][1]
+				out << "|" << setw(12) << right << jsonVector[it][0] << "|"
+					<< setw(12) << right << jsonVector[it][1]
 					<< "| " << setw(4) << left << jsonVector[it][2] << " |"
 					<< setw(16) << right << jsonVector[it][3] << "|"
 					<< setw(28) << right << jsonVector[it][4] << "|" << endl
-					<< setw(72) << "------------------------------------------------------------------------" << endl;
+					<< setw(80) << "--------------------------------------------------------------------------------" << endl;
 			}
 		}
 
 		else if (outSort == "batter") {
-			out << setw(72) << "|     batter     |  type  |  name  |  id  |           topping          |"<< endl;
-			out << setw(72) << "------------------------------------------------------------------------"<< endl;
+			out << setw(80) << "|     batter     |    type    |    name    |  id  |           topping          |"<< endl;
+			out << setw(80) << "--------------------------------------------------------------------------------"<< endl;
 
 			for (unsigned int it = 0; it < jsonVector.size(); it++) {
 				out << "|" << setw(16) << right << jsonVector[it][0] << "|"
-					<< setw(8) << right << jsonVector[it][1] << "|"
-					<< setw(8) << right << jsonVector[it][2]
+					<< setw(12) << right << jsonVector[it][1] << "|"
+					<< setw(12) << right << jsonVector[it][2]
 					<< "| " << setw(4) << left << jsonVector[it][3] << " |"
 					<< setw(28) << right << jsonVector[it][4] << "|" << endl
-					<< setw(72) << "------------------------------------------------------------------------" << endl;
+					<< setw(80) << "--------------------------------------------------------------------------------" << endl;
 			}
 		}
 
 		else if (outSort == "topping") {
-			out << setw(72) << "|           topping          |  type  |  name  |     batter     |  id  |"<< endl;
-			out << setw(72) << "------------------------------------------------------------------------"<< endl;
+			out << setw(80) << "|           topping          |    type    |    name    |     batter     |  id  |"<< endl;
+			out << setw(80) << "--------------------------------------------------------------------------------"<< endl;
 
 			for (unsigned int it = 0; it < jsonVector.size(); it++) {
 				out << "|" << setw(28) << right << jsonVector[it][0] << "|"
-					<< setw(8) << right << jsonVector[it][1] << "|"
-					<< setw(8) << right << jsonVector[it][2] << "|"
+					<< setw(12) << right << jsonVector[it][1] << "|"
+					<< setw(12) << right << jsonVector[it][2] << "|"
 					<< setw(16) << right << jsonVector[it][3]
-					<< "| " << setw(4) << left << jsonVector[it][1] << " |" << endl
-					<< setw(72) << "------------------------------------------------------------------------" << endl;
+					<< "| " << setw(4) << left << jsonVector[it][4] << " |" << endl
+					<< setw(80) << "--------------------------------------------------------------------------------" << endl;
 			}
 		}
 
-		else {
-			ofstream err("Error.txt");
-			err << "Wrong sorting type input argument in the command line";
-			exit(1);
-		}
+		else
+			throw InvalidSortArgumentException();
 		out.close();
 	}
 }
@@ -176,19 +185,11 @@ json getjFile(std::string filename) {
 
 			Sample.close();
 		}
-		else {
-			ofstream err;
-			err.open("Error.txt");
-			err<< "Input file could not be opened" <<endl;
-			err.close();
-		}
+		else
+			throw OpenFileException();
 	}
-	else {
-		ofstream err;
-		err.open("Error.txt");
-		err<< "Input file could not be opened" <<endl;
-		err.close();
-	}
+	else
+		throw OpenFileException();
 
 	return jfile;
 }
@@ -238,11 +239,8 @@ vector<vector<string>> sortByInput(vector<vector<string>> jsonVector, string arg
 	else if (arg == "topping")
 		type = 4;
 
-	else {
-		ofstream err("Error.txt");
-		err << "Wrong sorting type input argument in the command line";
-		exit(1);
-	}
+	else
+		throw InvalidSortArgumentException();
 
 	//Swaps the id with the chosen type
 	for (unsigned int it = 0; it < jsonVector.size(); it++) {
@@ -258,17 +256,17 @@ vector<vector<string>> selectionSortVector(vector<vector<string>> jsonVector) {
 
 	unsigned int min;
 
-	for (unsigned int size = 0; size < jsonVector.size(); size++) {
-		min = size;
+	for (unsigned int pos = 0; pos < jsonVector.size(); pos++) {
+		min = pos;
 
-		for (unsigned int it = 0; it < jsonVector.size(); it++) {
+		for (unsigned int it = pos; it < jsonVector.size(); it++) {
 			if (jsonVector[min][0] > jsonVector[it][0]) {
 				min = it;
 			}
 		}
 
-		vector<string> tempV = jsonVector[size];
-		jsonVector[size] = jsonVector[min];
+		vector<string> tempV = jsonVector[pos];
+		jsonVector[pos] = jsonVector[min];
 		jsonVector[min] = tempV;
 
 	}
